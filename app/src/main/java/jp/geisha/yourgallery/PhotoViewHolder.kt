0d7aafd19c64dfc73cd930510b8c53b7ac6f95e1
Tsutou <tsutoutakehara@gmail.com>
2020-06-25 17:52:46 +0900

    add Coil

--- app/src/main/java/jp/geisha/yourgallery/PhotoViewHolder.kt
+++ app/src/main/java/jp/geisha/yourgallery/PhotoViewHolder.kt
@@ -3,8 +3,20 @@ package jp.geisha.yourgallery
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import coil.api.load
+import coil.request.CachePolicy
+import jp.geisha.yourgallery.R.layout.list_item_photo
+import kotlinx.android.synthetic.main.list_item_photo.view.*
 
 class PhotoViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
     LayoutInflater.from(parent.context)
-        .inflate(R.layout.list_item_photo, parent, false)
-)
+        .inflate(list_item_photo, parent, false)
+) {
+    fun bind(photo: Photo) {
+        itemView.image.load(photo.uri){
+            crossfade(true)
+            placeholder(R.drawable.ic_launcher_foreground)
+            memoryCachePolicy(CachePolicy.READ_ONLY)
+        }
+    }
+}

6062796e1586f595e914a8adc2577a2e5f2378f0
Tsutou <tsutoutakehara@gmail.com>
2020-06-29 01:07:41 +0900

    Coil to Glide

--- app/src/main/java/jp/geisha/yourgallery/gallery/PhotoViewHolder.kt
+++ app/src/main/java/jp/geisha/yourgallery/gallery/PhotoViewHolder.kt
@@ -3,24 +3,29 @@ package jp.geisha.yourgallery.gallery
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import coil.api.load
-import coil.request.CachePolicy
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.RequestOptions
 import jp.geisha.yourgallery.R
 import jp.geisha.yourgallery.R.layout.list_item_photo
 import jp.geisha.yourgallery.entity.Photo
-import kotlinx.android.synthetic.main.list_item_photo.view.*
+import kotlinx.android.synthetic.main.list_item_photo.view.image
 
 class PhotoViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
     LayoutInflater.from(parent.context)
         .inflate(list_item_photo, parent, false)
 ) {
     fun bind(photo: Photo) {
-        itemView.image.apply {
-            load(photo.detail.uri) {
-                crossfade(true)
-                placeholder(R.drawable.ic_launcher_foreground)
-                memoryCachePolicy(CachePolicy.ENABLED)
-            }
-        }
+        Glide.with(itemView)
+            .load(photo.detail.uri)
+            .transition(DrawableTransitionOptions.withCrossFade())
+            .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
+            .apply(
+                RequestOptions()
+                    .centerCrop()
+                    .placeholder(R.drawable.ic_launcher_foreground)
+            )
+            .into(itemView.image)
     }
 }
